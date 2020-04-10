757c36b4e1a2897ff93925930ad5115d0ae9598c
sharpeningap <63176402+sharpeningap@users.noreply.github.com>
2020-04-10 18:32:39 -0400

    add function

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Alex");
+
+    double l = 5;
+    System.out.println("Ploshadi preamougol " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Ploshadi preamougolinika so storonami " + a + " i " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody +  "!");
+  }
+
+  public static double area(double len) {
+    return len*len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
