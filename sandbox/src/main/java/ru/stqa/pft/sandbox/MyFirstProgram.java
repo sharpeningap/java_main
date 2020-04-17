a0c91f0c5754b1f4e7fb7fd93a6eddbb6f932c4b
sharpeningap <63176402+sharpeningap@users.noreply.github.com>
2020-04-17 10:43:27 -0400

    try test

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,10 +8,10 @@ public static void main(String[] args) {
         hello("Alex");
 
         Square s = new Square(5);
-        System.out.println("Ploshadi preamougol " + s.l + " = " + area(s));
+        System.out.println("Ploshadi preamougol " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println("Ploshadi preamougolinika so storonami " + r.a + " i " + r.b + " = " + area(r));
+        System.out.println("Ploshadi preamougolinika so storonami " + r.a + " i " + r.b + " = " + r.area());
         }
 
 public static void hello(String somebody) {
@@ -19,6 +19,7 @@ public static void hello(String somebody) {
         }
 
 public static double area(Square s) {
+
         return s.l * s.l;
         }
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,8 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.annotations.Test;
+
 public class SquareTests {
+
+  @Test
   public void testArea() {
     Square s = new Square(5);
-    s.area();
+    assert s.area() == 25;
   }
 }
